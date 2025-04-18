6d471df06aad85b64c00c0144aa44669df8ada12
Raztreuzz <3385499862007@ingenieria.usac.edu.gt>
2025-04-18 09:13:30 -0600

    ProyectFX

---
+++ ecg-realtime/src/main/java/com/raz/ecg/App.java
@@ -0,0 +1,13 @@
+package com.raz.ecg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
